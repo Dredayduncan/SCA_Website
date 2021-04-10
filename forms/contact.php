6f959829eee1f22913b9fd1991b28f2657a7e42f
Dredayduncan <dredayduncan@gmail.com>
2021-04-10 17:53:39 +0000

    completed version

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'dredayduncan@yahoo.com';
+  $receiving_email_address = 'info@spearcapitaladvisory.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -17,25 +17,58 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  // SMTP Credentials and info
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => "smtp.gmail.com",
+    'username' => $receiving_email_address,
+    'password' => '@Ydd3cp!jTgpoUb',
     'port' => '587'
   );
-  */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  if (isset($_POST['name'])){
+    
+    $contact->to = $receiving_email_address;
+    $contact->from_name = $_POST['name'];
+    $contact->from_email = $_POST['email'];
+    $contact->subject = $_POST['subject'];
+  
 
-  echo $contact->send();
+    $contact->add_message( $_POST['name'], 'From');
+    $contact->add_message( $_POST['email'], 'Email');
+    $contact->add_message( $_POST['message'], 'Message', 10);
+
+    echo $contact->send();
+  }
+
+  else {
+    $mail = new PHPMailer();
+
+    // Settings
+    $mail->IsSMTP();
+    $mail->CharSet = 'UTF-8';
+    $mail->Host = 'smtp.gmail.com';
+    
+    $mail->SMTPDebug = 0;                     // enables SMTP debug information (for testing)
+    $mail->SMTPAuth = true;                  // enable SMTP authentication
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;                    // set the SMTP port for the GMAIL server
+    $mail->Username = $receiving_email_address; // SMTP account username example
+    $mail->Password = "@Ydd3cp!jTgpoUb";     
+
+    $mail->setFrom($receiving_email_address);
+    $mail->addAddress($receiving_email_address); 
+
+    // Content
+    //$mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Newsletter Inclusion';
+    $mail->Body    = 'Kindly include me in the newsletter using '. $_POST['newsEmail'];
+    // $mail->AltBody = 'Kindly include me in the newsletter using '. $_POST['newsEmail'];
+
+    echo $mail->send();
+    header("Location: ../index.html#footer");
+
+  }
+
+  
 ?>
